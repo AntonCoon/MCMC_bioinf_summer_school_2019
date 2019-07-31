0e6da96aa5e0bc430641d27bf7254d3627c63693
AntonCoon <eliseevantoncoon@gmail.com>
2019-07-31 21:41:14 +0300

    Gauss practice

--- 1_MC/Gauss.py
+++ 1_MC/Gauss.py
@@ -1,10 +1,12 @@
 import random
-from collections import Counter
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 
 def suggest(x):
-    return x + eps * (2 * random.random() - 1)
+    # Самостоятельно
 
 
 def prob(old_x, new_x):
@@ -13,27 +15,28 @@ def prob(old_x, new_x):
 
 def mcmc(init=0):
     x = init
-    res = []
     for i in range(n_steps):
         new_x = suggest(x)
         if random.random() < prob(x, new_x):
             x = new_x
-        res.append(x)
-    return res
+    return x
 
 
 if __name__ == '__main__':
-    eps = 0.1
-    n_steps = 5000
+    eps = 1
+    n_steps = 1500
 
     distr = []
     for j in range(1000):
         distr.append(mcmc())
 
-    conv = []
-    freq = []
-    for idx in range(n_steps):
-        distr_on_step = [line[idx] for line in distr]
-        freq = Counter(distr_on_step)
+    distr = np.array(distr)
 
-    print(distr_on_step)
+    mu = 0
+    variance = eps
+    sigma = math.sqrt(variance)
+    x = np.linspace(0, 10, 100)
+    plt.plot(x, 2 * norm.pdf(x, mu, sigma), '--')
+
+    plt.hist(distr, normed=True)
+    plt.show()
